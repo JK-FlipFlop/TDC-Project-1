b7ed0102919902fc7070235344fdfebcdc2dd61a
JK-FlipFlop <jaikapur98@gmail.com>
2019-02-10 19:10:26 +0530

    Final Commit

---
+++ login.js
@@ -0,0 +1,18 @@
+function login(){
+
+  window.location = 'https://google.com';
+  /*let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+
+  if(username == "admin" && password == "root")
+  {
+    window.location.href = 'https://google.com'
+    alert("success");
+  }
+
+  else{
+    console.log(username);
+    console.log(password);
+    alert("Invalid username or password");
+  }*/
+}
